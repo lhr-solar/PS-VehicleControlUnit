0d78d302f7b0b1a8de3af9780cf44841d0369008
Gavin Wang <gavinwang07@gmail.com>
2026-03-02 22:06:29 -0600

    For Tony (Gavin)

--- Firmware/Tasks/Inc/InitTask.h
+++ Firmware/Tasks/Inc/InitTask.h
@@ -1,10 +1,7 @@
 #pragma once
 
-#include "StatusLEDs.h"
 #include "FaultHandlerTask.h"
 #include "PrechargeTask.h"
-#include "PrechargeTask.h"
-#include "FaultHandlerTask.h"
 
 extern StaticTask_t FaultHandlerTask_Buffer;
 extern StackType_t FaultHandlerTask_Stack[configMINIMAL_STACK_SIZE];
